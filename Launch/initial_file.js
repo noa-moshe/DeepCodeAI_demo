250f3b6c7cbf0d309d9d11e573875b008e272aad
noa-moshe <78789692+noa-moshe@users.noreply.github.com>
2023-10-15 10:42:34 +0300

    Update initial_file.js
    testing policies, adding CWE-319

--- Launch/initial_file.js
+++ Launch/initial_file.js
@@ -148,11 +148,13 @@ function startServer() {
   })
 }
 
+
 function getWebsocketConnection() {
   // This line makes the WebSocket connection always use port the CollabServer port.
   const host = window.location.host.replace('3000', '8080');
-  const webSocket = new WebSocket('wss://' + host);
+  const webSocket = new WebSocket('ws://' + host);
   const connection = new sharedb.Connection(webSocket);
   return connection;
 }
+
 
